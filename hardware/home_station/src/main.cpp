cc047c43f25456450524b289e141a62ef21140e9
Ry <ry.limitless@gmail.com>
2024-03-01 16:32:06 -0500

    dht and bmp

--- hardware/home_station/src/main.cpp
+++ hardware/home_station/src/main.cpp
@@ -1,18 +1,56 @@
 #include <Arduino.h>
+#include <DHT.h>
+#include <Adafruit_BMP280.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define dhtPin 23
+#define dhtType DHT22
+
+//Contructors
+
+Adafruit_BMP280 bmp; // I2C
+DHT dht(dhtPin, dhtType);
+
+bool checkDHT(float t, float h);
+
+//TODO: add error checking for the DHT sensor
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  dht.begin();
+  while(!bmp.begin(0x76)){
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    delay(1000);
+  }
+
+
 }
 
 void loop() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float p = bmp.readPressure();
+  float a = bmp.readAltitude(1013.25);
+  float t2 = bmp.readTemperature();
+
+  float temp = (t + t2) / 2;
+  if (!checkDHT(t, h)){
+    Serial.println("Error reading from DHT sensor");
+    
+  }else {
+  Serial.printf("Temperature %.2f C \t Humidity %.2f\n", temp, h);
+  Serial.printf("Pressure %.2f Pa \t Approximate Altitude %.2f\n", p, a);
+
+  }
+  delay(2000);
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+//Function to check dht readings
+
+bool checkDHT(float t, float h){
+  if (isnan(t) || isnan(h)){
+    return false;
+  }
+  return true;
 }
